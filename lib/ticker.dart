a1f79f118073ef3d712228cbde3560cdf65e980a
Hope Madziakapita <hopemadziakapita@gmail.com>
2024-02-21 14:46:19 +0000

    Update ticker.dart

--- lib/ticker.dart
+++ lib/ticker.dart
@@ -1,11 +1,7 @@
-//Class declaration
 class Ticker {
-
-  //constant constructor:::: instances of the class will be created at compile time
   const Ticker();
-
-  //Methond to return stream of integers
-  Stream<int> tick ({required int ticks}){
-    return Stream.periodic(const Duration(seconds: 1), (x) => ticks - x -1).take(ticks);
+  Stream<int> tick({required int ticks}) {
+    return Stream.periodic(const Duration(seconds: 1), (x) => ticks - x - 1)
+        .take(ticks);
   }
 }
